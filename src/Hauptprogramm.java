3b463dfdf9c29b473583b69dfc55760b547cf065
s.kuebel <s.kuebel@otterbach.de>
2017-08-03 10:26:17 +0200

    Weiterer Test, sollte endlich Funktionieren

--- src/Hauptprogramm.java
+++ src/Hauptprogramm.java
@@ -4,6 +4,9 @@ public class Hauptprogramm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Welt");
+		for(int x = 0; x <= 10; x++) {
+			System.out.println(x);
+		}
 	}
 
 }
