85fb55d9437449494395f5c4290e5543b25ea06d
s.kuebel <s.kuebel@otterbach.de>
2017-08-03 10:47:44 +0200

    Summe der Zahlenmenge

--- src/Hauptprogramm.java
+++ src/Hauptprogramm.java
@@ -1,11 +1,19 @@
+import com.sun.org.apache.xerces.internal.impl.xs.identity.Selector.Matcher;
+
+import jdk.nashorn.internal.runtime.regexp.joni.MatcherFactory;
 
 public class Hauptprogramm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] zahlenmenge = {44,56,4,23,34,5,7,9,123,78};
+		double[] zahlenmenge = {44,56,4,23,34,5,7,9,123,78};
+		double summe = 0;
 		
+		for(int x = 0; x < zahlenmenge.length; x++) {
+			summe += zahlenmenge[x];
+		}
 		
+		System.out.println(summe);
 	}
 
 }
